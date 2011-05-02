490b1ddd1591693c21d90a7881b005be4311ab2e
Roger Turnau <rturnau@gmail.com>
2011-05-02 14:03:51 -0400

    figured out how to center the zero coords and calculate distance from edge using dot product.

--- main.lua
+++ main.lua
@@ -13,6 +13,8 @@ function init_planes()
         {x = 0,  y = 1, offset = love.graphics.getHeight()/2 - TOP_OFFSET   },
         {x = 0, y = -1, offset = love.graphics.getHeight()/2 - BOTTOM_OFFSET}
     }
+    
+    
 end
 
 function love.load()
@@ -20,26 +22,26 @@ function love.load()
 
     init_planes()
 
-    circle1 = { x = 79,   y = 215, speedX = -10, speedY = -10 }
-    circle2 = { x = 310,  y = 557, speedX = 58,   speedY = 19   }
-    circle3 = { x = 2,    y = 80,  speedX = -136, speedY = -73  }
-    circle4 = { x = 469,  y = 313, speedX = 46,   speedY = -94  }
-    circle5 = { x = 416,  y = 561, speedX = 120,  speedY = 90   }
-    circle6 = { x = 313,  y = 174, speedX = 15,   speedY = -117 }
-    circle7 = { x = 209,  y = 440, speedX = 18,   speedY = -91  }
-    circle8 = { x = 323,  y = 456, speedX = -14,  speedY = 102  }
-    circle9 = { x = 529,  y = 422, speedX = -29,  speedY = 130  }
-    circle10 = { x = 445, y = 539, speedX = 149,  speedY = 61   }
-    circle11 = { x = 357, y = 7,   speedX = -62,  speedY = 145  }
-    circle12 = { x = 355, y = 22,  speedX = 129,  speedY = 53   }
-    circle13 = { x = 294, y = 262, speedX = -13,  speedY = -98  }
-    circle14 = { x = 562, y = 352, speedX = 48,   speedY = 11   }
-    circle15 = { x = 761, y = 566, speedX = -69,  speedY = 77   }
-    circle16 = { x = 353, y = 306, speedX = 14,   speedY = -82  }
-    circle17 = { x = 531, y = 513, speedX = 42,   speedY = 33   }
-    circle18 = { x = 50,  y = 108, speedX = -12,  speedY = 69   }
-    circle19 = { x = 392, y = 267, speedX = 100,  speedY = 141  }
-    circle20 = { x = 602, y = 486, speedX = -35,  speedY = 52   }
+    circle1 = { x = -100,   y = -300, speedX = -50, speedY = -75 }
+    circle2 = { x = 10,  y = 257, speedX = 58,   speedY = 19   }
+    circle3 = { x = -298,    y = -220,  speedX = -136, speedY = -73  }
+    circle4 = { x = 169,  y = 13, speedX = 46,   speedY = -94  }
+    circle5 = { x = 116,  y = 261, speedX = 120,  speedY = 90   }
+    circle6 = { x = 13,  y = -126, speedX = 15,   speedY = -117 }
+    circle7 = { x = -91,  y = 140, speedX = 18,   speedY = -91  }
+    circle8 = { x = 23,  y = 156, speedX = -14,  speedY = 102  }
+    circle9 = { x = 229,  y = 122, speedX = -29,  speedY = 130  }
+    circle10 = { x = 145, y = 239, speedX = 149,  speedY = 61   }
+    circle11 = { x = 57, y = -293,   speedX = -62,  speedY = 145  }
+    circle12 = { x = 55, y = -278,  speedX = 129,  speedY = 53   }
+    circle13 = { x = -6, y = -38, speedX = -13,  speedY = -98  }
+    circle14 = { x = 262, y = 52, speedX = 48,   speedY = 11   }
+    circle15 = { x = 461, y = 266, speedX = -69,  speedY = 77   }
+    circle16 = { x = 53, y = 6, speedX = 14,   speedY = -82  }
+    circle17 = { x = 231, y = 213, speedX = 42,   speedY = 33   }
+    circle18 = { x = -250,  y = -192, speedX = -12,  speedY = 69   }
+    circle19 = { x = 92, y = -33, speedX = 100,  speedY = 141  }
+    circle20 = { x = 302, y = 186, speedX = -35,  speedY = 52   }
     circles = {
                 circle1 } -- , circle2, circle3, circle4, circle5,
                 --circle6, circle7, circle8, circle9, circle10,
@@ -51,16 +53,16 @@ end
 
 function love.update(dt)
     for key, value in pairs(circles) do
-        if (value.x < -1) and (value.speedX < 0) then
+        if (value.x < -(love.graphics.getWidth()/2)) and (value.speedX < 0) then
             value.speedX = -value.speedX
         end
-        if (value.x > love.graphics.getWidth()) and (value.speedX > 0) then
+        if (value.x > love.graphics.getWidth()/2) and (value.speedX > 0) then
             value.speedX = -value.speedX
         end
-        if (value.y < -1) and (value.speedY < 0) then
+        if (value.y < -(love.graphics.getHeight()/2)) and (value.speedY < 0) then
             value.speedY = -value.speedY
         end
-        if (value.y > love.graphics.getHeight()) and (value.speedY > 0) then
+        if (value.y > love.graphics.getHeight()/2) and (value.speedY > 0) then
             value.speedY = -value.speedY
         end
 
@@ -71,22 +73,21 @@ end
 
 function check_collision()
 
-    local planedist = 0
+    local planedist = -150
 
     for key, circle in pairs(circles) do
         for key2, plane in pairs(planes) do
             planedist = planedist + 50
             --love.graphics.print(plane.x.." "..plane.y.." "..plane.offset, 300, planedist)
             --print(plane.x.." "..plane.y.." "..plane.offset)
+            
             local distance = dot_product(circle.x, circle.y, plane.x, plane.y) + plane.offset
-            love.graphics.print("distance = "..distance.." plane x= "..plane.x..", plane y= "..plane.y, 100, planedist)
+            love.graphics.print("distance = "..distance.." plane x= "..plane.x..", plane y= "..plane.y, -150, planedist)
 
             if distance < 0 then
 
-                --print("circle at ("..circle.x..", "..circle.y..") hit the edge")
-
-
-                --print("distance = "..distance)
+                print("circle at ("..circle.x..", "..circle.y..") hit the edge")
+                
             end
         end
     end
@@ -117,7 +118,7 @@ end
 
 
 function love.draw()
-
+    love.graphics.translate(300, 300)
     check_collision()
 
     for key, value in pairs(circles) do
